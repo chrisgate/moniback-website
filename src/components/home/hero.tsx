673a85bc1e797dd8fb540503f905164c91d77234
TPriince <tobiloba110@gmail.com>
2025-07-28 14:05:49 +0100

    fix: remove background color from hero section

--- src/components/home/hero.tsx
+++ src/components/home/hero.tsx
@@ -2,7 +2,7 @@ import { Button } from "../ui/button";
 
 export default function Hero() {
   return (
-    <section className="h-[calc(100vh-var(--header-height))] bg-red-300 pt-20">
+    <section className="h-[calc(100vh-var(--header-height))] pt-20">
       <div className="flex flex-col items-center gap-y-[30px] px-4 md:px-6">
         <div className="mx-auto max-w-[781px] space-y-[18px] text-center">
           <h1 className="text-[4.24rem] leading-[76px] font-bold tracking-[-2px]">
