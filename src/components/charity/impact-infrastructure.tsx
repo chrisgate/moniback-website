9ffd3da50725bdda94d56392654c81caa2cd9dbd
TPriince <tobiloba110@gmail.com>
2025-08-18 10:26:55 +0100

    fix: Reviews

--- src/components/charity/impact-infrastructure.tsx
+++ src/components/charity/impact-infrastructure.tsx
@@ -2,7 +2,7 @@ import Container from "../ui/container";
 
 export default function ImpactInfrastructure() {
   return (
-    <section className="bg-Primary-300 bg-[url(/images/impact-infrastructure.png)] bg-no-repeat">
+    <section className="bg-Primary-300 bg-[url(/images/impact-infrastructure.png)] bg-contain bg-no-repeat">
       <Container className="flex min-h-[515px] items-center py-16 lg:min-h-[748px] lg:justify-end">
         <p className="w-[632px] text-[2rem] font-bold tracking-[-2px] lg:text-5xl">
           We turn loyalty tech into impact infrastructure enabling governments,
